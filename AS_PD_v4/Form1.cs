8c426261e669a52d4a77068dbca3723645369a51
Adrians Stepe <as3616@gymn.local>
2024-02-27 15:04:58 +0200

    pirmās divas pogas gatavas

--- AS_PD_v4/Form1.cs
+++ AS_PD_v4/Form1.cs
@@ -16,5 +16,33 @@ namespace AS_PD_v4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ienākumi = Convert.ToDouble(textBox1.Text);
+            double izdevumi = Convert.ToDouble(textBox2.Text);
+            double kop = ienākumi + izdevumi;
+            textBox3.Text = kop + " EUR";
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double viens = Convert.ToDouble(textBox4.Text);
+            double divi = Convert.ToDouble(textBox5.Text);
+            double trīs = Convert.ToDouble(textBox6.Text);
+            double Četri = Convert.ToDouble(textBox7.Text);
+            double izdevumi1 = viens + divi + trīs + Četri;
+            textBox9.Text = izdevumi1 + " EUR";
+        }
     }
 }
